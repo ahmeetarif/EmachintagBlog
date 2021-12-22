7c74b3f9366ba03da9ae4e8f5582a2684eb0ad41
ahmeetarif <ahmetarif1254@gmail.com>
2021-12-22 15:15:41 +0300

    Added BaseEntity

---
+++ src/client/EmachintagBlog.Client.WebApp/Common/Core/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmachintagBlog.Client.WebApp.Common.Core
+{
+    public partial class BaseEntity
+    {
+        public long Id { get; set; }
+        public DateTime CreatetdAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
