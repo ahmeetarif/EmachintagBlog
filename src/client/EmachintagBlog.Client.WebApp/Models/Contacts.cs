1da32f880b35c9aecbc31f4ae3fad3fed4bac3ba
ahmeetarif <ahmetarif1254@gmail.com>
2021-12-22 15:15:52 +0300

    Added Contacts Entity

---
+++ src/client/EmachintagBlog.Client.WebApp/Models/Contacts.cs
@@ -0,0 +1,15 @@
+﻿using EmachintagBlog.Client.WebApp.Common.Core;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmachintagBlog.Client.WebApp.Models
+{
+    public partial class Contacts : BaseEntity
+    {
+        public string UserId { get; set; }
+
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
