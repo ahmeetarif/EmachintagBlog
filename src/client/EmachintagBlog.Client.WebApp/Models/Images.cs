aebe84aca3e5180352656e7294c32ac7ab4796b9
ahmeetarif <ahmetarif1254@gmail.com>
2021-12-22 15:16:15 +0300

    Added Images Entity

---
+++ src/client/EmachintagBlog.Client.WebApp/Models/Images.cs
@@ -0,0 +1,12 @@
+﻿using EmachintagBlog.Client.WebApp.Common.Core;
+
+namespace EmachintagBlog.Client.WebApp.Models
+{
+    public partial class Images : BaseEntity
+    {
+        public long BlogId { get; set; }
+
+        public string ImagePath { get; set; }
+        public bool IsCover { get; set; }
+    }
+}
