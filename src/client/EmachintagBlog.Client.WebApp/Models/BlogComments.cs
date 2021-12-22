e8c18a84e0948fab0e290ed3dd2261f951a59fe9
ahmeetarif <ahmetarif1254@gmail.com>
2021-12-22 15:16:29 +0300

    Added BlogComments Entity

---
+++ src/client/EmachintagBlog.Client.WebApp/Models/BlogComments.cs
@@ -0,0 +1,13 @@
+﻿using EmachintagBlog.Client.WebApp.Common.Core;
+
+namespace EmachintagBlog.Client.WebApp.Models
+{
+    public partial class BlogComments : BaseEntity
+    {
+        public long BlogId { get; set; }
+        public long CommentParentId { get; set; }
+
+        public string UserId { get; set; }
+        public string CommentMessage { get; set; }
+    }
+}
