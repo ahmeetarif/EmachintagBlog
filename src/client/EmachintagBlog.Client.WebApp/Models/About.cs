349f9abd24603b663c2147045c80e59d6ec9be92
ahmeetarif <ahmetarif1254@gmail.com>
2021-12-22 15:16:01 +0300

    Added About Entity

---
+++ src/client/EmachintagBlog.Client.WebApp/Models/About.cs
@@ -0,0 +1,16 @@
+﻿using EmachintagBlog.Client.WebApp.Common.Core;
+
+namespace EmachintagBlog.Client.WebApp.Models
+{
+    public partial class About : BaseEntity
+    {
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+
+        public string InstagramLink { get; set; }
+        public string TwitterLink { get; set; }
+        public string FacebookLink { get; set; }
+        public string YoutubeLink { get; set; }
+    }
+}
