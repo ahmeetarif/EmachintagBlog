a50c5d599b61cd03cc26831a81879f7b8598991c
ahmeetarif <ahmetarif1254@gmail.com>
2021-12-22 15:03:34 +0300

    Added ApplicationUser Entity

---
+++ src/client/EmachintagBlog.Client.WebApp/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿namespace EmachintagBlog.Client.WebApp.Models
+{
+    using EmachintagBlog.Client.WebApp.Common.Enums;
+    using Microsoft.AspNetCore.Identity;
+
+    public partial class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; }
+        public UserTypeEnum Type { get; set; }
+    }
+}
